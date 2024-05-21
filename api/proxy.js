10163e53578f184caa43a28877477932b99938d8
曹智铭 <czm@czhiming.cn>
2024-05-21 13:50:00 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://invidious.jing.rocks";//your website url
+  let target = "https://opentunnel.net";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
