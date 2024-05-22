469d7e92df02ce2264019758cf41a15e0cb2c608
曹智铭 <czm@czhiming.cn>
2024-05-22 13:41:57 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
 
   // 检查是否存在query参数
   if (req.query.target) {
-    target = req.query.target; // 如果存在query参数，则将其赋值给target变量
+    target ="https://WWW.GOOGLE.COM"; // 如果存在query参数，则将其赋值给target变量
   } else {
     // 如果不存在query参数，则设置默认的目标URL
     target = "https://www.google.com";
