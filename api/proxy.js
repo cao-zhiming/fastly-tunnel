d1f6658df27a6ec0c93e0fece7ffa133a9555411
曹智铭 <czm@czhiming.cn>
2024-05-22 11:43:01 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://invidious.jing.rocks";//your website url
+  let target = "https://q2.tangziyan2026.workers.dev";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
