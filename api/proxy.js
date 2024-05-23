3234b4b81f4a5a80769d1cb414c3843acce307f8
曹智铭 <czm@czhiming.cn>
2024-05-23 09:24:43 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,10 @@ module.exports = (req, res) => {
 
   // 检查是否存在query参数
   if (req.query.target) {
-    target ="https://WWW.bilibili.COM"; // 如果存在query参数，则将其赋值给target变量
+    target ="https://search.bilibili.COM"; // 如果存在query参数，则将其赋值给target变量
   } else {
     // 如果不存在query参数，则设置默认的目标URL
-    target = "https://www.bilibili.com";
+    target = "https://search.bilibili.com";
   }
 
   // 创建代理中间件
