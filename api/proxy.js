18e77007aff2009cf16fba0b2aa257f4689209af
曹智铭 <czm@czhiming.cn>
2024-05-23 09:12:30 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,10 @@ module.exports = (req, res) => {
 
   // 检查是否存在query参数
   if (req.query.target) {
-    target ="https://WWW.GOOGLE.COM"; // 如果存在query参数，则将其赋值给target变量
+    target ="https://github.COM"; // 如果存在query参数，则将其赋值给target变量
   } else {
     // 如果不存在query参数，则设置默认的目标URL
-    target = "https://www.google.com";
+    target = "https://github.com";
   }
 
   // 创建代理中间件
